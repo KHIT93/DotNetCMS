774e97a01282cb7687a89d44eb19e5eb4572135c
Kenneth Hansen <kenneth@khansen-it.dk>
2016-02-25 11:33:13 +0100

    Initial Commit

---
+++ DotNetCMS.Data.Models/Page.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCMS.Data.Models
+{
+    public class Page
+    {
+    }
+}
---
+++ DotNetCMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetCMS.MvcApplication" Language="C#" %>
